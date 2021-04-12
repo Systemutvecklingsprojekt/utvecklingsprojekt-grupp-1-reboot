3dcba6895e210a0047d717d40f4e860ff5f6e7ef
Josef <jamaihaitot.j@gmail.com>
2021-04-12 22:28:51 +0200

    Added fetchColumn() :+1: Returns ArrayList()

--- src/projekt/helpers/Database.java
+++ src/projekt/helpers/Database.java
@@ -6,55 +6,73 @@ package projekt.helpers;
  * and open the template in the editor.
  */
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author Josef
  */
-public class Database {
-    
-    private static Connection conn = null;
-    private static final String url = "jdbc:mysql://jamaihaitot.com:2230/development";
-    private static final String user = "development";
-    private static final String password = "tUG8p6xQsXwEXYS8";
-    
-    private Database() {
-        
-    }
-    
-    
-    public static int executeUpdate(String query) throws SQLException {
-        Statement stat = getConn().createStatement();
-        stat.closeOnCompletion();
-        return stat.executeUpdate(query);
-    }
+public class Database
+{
+	
+	private static Connection conn = null;
+	private static final String CONNECTION_STRING = "jdbc:mysql://jamaihaitot.com:2230/development";
+	private static final String USERNAME = "development";
+	private static final String PASSWORD = "tUG8p6xQsXwEXYS8";
+	
+	private Database()
+	{
+		
+	}
+	
+	public static int executeUpdate(String query) throws SQLException
+	{
+		Statement stat = getConn().createStatement();
+		stat.closeOnCompletion();
+		return stat.executeUpdate(query);
+	}
+	
+	public static String fetchSingle(String query) throws SQLException
+	{
+		Statement stat = getConn().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		stat.closeOnCompletion();
+		String single = null;
+		ResultSet rs = stat.executeQuery(query);
+		if (rs.next()) {
+			single = rs.getString(1);
+		}
+		return single;
+	}
 
-    public static String fetchSingle(String query) throws SQLException {
-        Statement stat = getConn().createStatement();
-        stat.closeOnCompletion();
-        String single = null;
-        ResultSet rs = stat.executeQuery(query);
-        if(rs.next()){
-            single = rs.getString(1);
-        }
-        return single;
-    }
+	public static ArrayList<String> fetchColumn(String query) throws SQLException
+	{
+		ResultSet rs;
+		rs = fetchRows(query);
+		ArrayList<String> columnList = new ArrayList<>();
+		while (rs.next()) {
+			columnList.add(rs.getNString(1));
+		}
+		return columnList;
+		
+	}
 
-    public static ResultSet fetchRows(String query) throws SQLException {
-        Statement stat = getConn().createStatement();
-        stat.closeOnCompletion();
-        return stat.executeQuery(query);
-    }
-    
-    public static Connection getConn() {
-        if (conn == null) {
-            try {
-                conn = DriverManager.getConnection(url, user, password);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return conn;
-    }
-    
+	public static ResultSet fetchRows(String query) throws SQLException
+	{
+		Statement stat = getConn().createStatement();
+		stat.closeOnCompletion();
+		return stat.executeQuery(query);
+	}
+	
+	public static Connection getConn()
+	{
+		if (conn == null) {
+			try {
+				conn = DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
+	
 }
