c4062edc3c30661a4d489e8719b4a3dfaa53fbb1
Josef <jamaihaitot.j@gmail.com>
2021-04-14 14:05:19 +0200

    Update Refaktor.java
    Refaktor är temporär samlingsplats för utbrytna funktioner

--- src/projekt/Refactor.java
+++ src/projekt/Refactor.java
@@ -5,11 +5,40 @@
  */
 package projekt;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Josef
  */
 public class Refactor
 {
+	public static DefaultTableModel tableModelBuilder(ResultSet rs) throws SQLException
+	{
+		ResultSetMetaData metaData = rs.getMetaData();
+
+		//kör igenom metadatan (ex hur många columner, och hämtar namnen)
+		Vector<String> columnNames = new Vector<String>();
+		int columnCount = metaData.getColumnCount();
+		for (int column = 1; column <= columnCount; column++) {
+			columnNames.add(metaData.getColumnName(column));
+		}
+
+		// data of the table
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		while (rs.next()) {
+			Vector<Object> vector = new Vector<Object>();
+			for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+				vector.add(rs.getObject(columnIndex));
+			}
+			data.add(vector);
+		}
+
+		return new DefaultTableModel(data, columnNames);
+	}
 	
 }
