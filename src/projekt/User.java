f062a8ee677a91256f6e3833d497483a49dc347d
Josef <jamaihaitot.j@gmail.com>
2021-04-14 12:52:01 +0200

    Refactor.java

--- src/projekt/User.java
+++ src/projekt/User.java
@@ -4,64 +4,72 @@
  * and open the template in the editor.
  */
 package projekt;
+
 import projekt.helpers.Database;
 
 /**
  *
  * @author Amand
  */
-public class User {
-    private int userID;
-    private String firstName;
-    private String lastName;
-    private String email; 
-    private String admin; 
-    private String password;
-    
+public class User
+{
 
-    public User(int id) {
-    this.userID = id;
-    createUser();
-    
-}
-    private void createUser() {
-        try {
-        this.firstName = Database.fetchSingle("Select FIRSTNAME from User where USERID = " + userID);
-        this.lastName = Database.fetchSingle("Select LASTNAME from User where USERID = " + userID);
-        this.password = Database.fetchSingle("Select PASSWORD from User where USERID = " + userID);
-        this.email = Database.fetchSingle("Select EMAIL from User where USERID = " + userID);
-        this.admin = Database.fetchSingle("Select ADMINSTATUS from User where USERID = " + userID);
-        }
-        catch (Exception e) {
-            System.out.print("User querrys fuckar ur");
-        }
-    
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    
-    public String getLastName(){
-        return lastName;
-    }
-    
-    public int getUserID(){
-        return userID;
-    }
-    
-    public String getEmail(){
-        return email;
-    }
-    
-    public String getAdmin(){
-        return admin;
-    }
-    
-    public String getPassword(){
-        return password;
-    }
-    
-    
-    
+	private int userID;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String admin;
+	private String password;
+
+	public User(int id)
+	{
+		this.userID = id;
+		createUser();
+
+	}
+
+	private void createUser()
+	{
+		try {
+			this.firstName = Database.fetchSingle("Select FIRSTNAME from User where USERID = " + userID);
+			this.lastName = Database.fetchSingle("Select LASTNAME from User where USERID = " + userID);
+			this.password = Database.fetchSingle("Select PASSWORD from User where USERID = " + userID);
+			this.email = Database.fetchSingle("Select EMAIL from User where USERID = " + userID);
+			this.admin = Database.fetchSingle("Select ADMINSTATUS from User where USERID = " + userID);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public int getUserID()
+	{
+		return userID;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public String getAdmin()
+	{
+		return admin;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
 }
