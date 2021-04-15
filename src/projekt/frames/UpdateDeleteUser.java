6f455e605fe4a2e8c02271efbec4190a9d05fd0b
Josef <jamaihaitot.j@gmail.com>
2021-04-15 13:46:50 +0200

    Ändrade metoden för att välja användare
    Nu skriver man inte in vilken användare man vill ändra eller ta bort

--- src/projekt/frames/UpdateDeleteUser.java
+++ src/projekt/frames/UpdateDeleteUser.java
@@ -58,31 +58,36 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 	 */
 	@SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    private void initComponents()
+    {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         deleteButton = new javax.swing.JButton();
         changeInformationButton = new javax.swing.JButton();
-        jtfID = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jBBack = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
+            new Object [][]
+            {
 
             },
-            new String [] {
+            new String []
+            {
                 "UserID", "Firstname", "Lastname", "Email", "Password", "Adminstatus"
             }
-        ) {
-            boolean[] canEdit = new boolean [] {
+        )
+        {
+            boolean[] canEdit = new boolean []
+            {
                 false, false, false, false, true, false
             };
 
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
+            public boolean isCellEditable(int rowIndex, int columnIndex)
+            {
                 return canEdit [columnIndex];
             }
         });
@@ -90,32 +95,32 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 
         deleteButton.setBackground(new java.awt.Color(204, 204, 255));
         deleteButton.setText("Ta bort");
-        deleteButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        deleteButton.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 deleteButtonActionPerformed(evt);
             }
         });
 
         changeInformationButton.setBackground(new java.awt.Color(204, 204, 255));
         changeInformationButton.setText("Ändra information");
-        changeInformationButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        changeInformationButton.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 changeInformationButtonActionPerformed(evt);
             }
         });
 
-        jtfID.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jtfIDActionPerformed(evt);
-            }
-        });
-
-        jLabel1.setText("Vänlige ange UserID på användaren du vill ändra eller ta bort");
+        jLabel1.setText("Vänligen marker en användare du vill ta bort eller ändra.");
 
         jBBack.setBackground(new java.awt.Color(204, 204, 255));
         jBBack.setText("Tillbaka");
-        jBBack.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jBBack.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 jBBackActionPerformed(evt);
             }
         });
@@ -135,9 +140,7 @@ public class UpdateDeleteUser extends javax.swing.JFrame
                                 .addComponent(jBBack, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jtfID)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, Short.MAX_VALUE))
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
                                 .addGap(36, 36, 36)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(deleteButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -155,9 +158,7 @@ public class UpdateDeleteUser extends javax.swing.JFrame
                     .addComponent(deleteButton)
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jtfID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(changeInformationButton))
+                .addComponent(changeInformationButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
                 .addComponent(jBBack)
                 .addGap(21, 21, 21))
@@ -167,16 +168,20 @@ public class UpdateDeleteUser extends javax.swing.JFrame
     }// </editor-fold>//GEN-END:initComponents
 
     private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
-		if (Validation.isInt(jtfID) && Validation.isStringNull(jtfID.getText())) {
-			int tempId = Integer.parseInt(jtfID.getText());
-			String updatePost = "Update Post SET UserID = 11 Where UserID = " + tempId;
-			String delUser = "Delete from User where UserID =" + tempId;
-			String delMeeting = "Delete from Meeting where UserID =" + tempId;
-			String delMetAt = "Delete from Meeting_Attandence where UserID =" + tempId;
-			String delPost = "Delete from Post where UserID =" + tempId;
-                        String delCom = "Delete from Comments where UserID =" + tempId;
-                        String updatCom = "Update Comments Set UserID = 11 Where UserID = " + tempId;
-                        
+		int userID = -1;
+		try {
+			userID = Refactor.getTableValueFirstColumn(table2);
+		
+
+		if (true) {
+			String updatePost = "Update Post SET UserID = 11 Where UserID = " + userID;
+			String delUser = "Delete from User where UserID =" + userID;
+			String delMeeting = "Delete from Meeting where UserID =" + userID;
+			String delMetAt = "Delete from Meeting_Attandence where UserID =" + userID;
+			String delPost = "Delete from Post where UserID =" + userID;
+			String delCom = "Delete from Comments where UserID =" + userID;
+			String updatCom = "Update Comments Set UserID = 11 Where UserID = " + userID;
+
 			if (idKoll() == false) {
 				return;
 			}
@@ -188,8 +193,8 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 
 					if (dialog2 == JOptionPane.YES_OPTION) {
 						Database.executeUpdate(updatePost);//set posts ownership to deleted user
-                                                Database.executeUpdate(updatCom);
-                                                Database.executeUpdate(delMetAt);
+						Database.executeUpdate(updatCom);
+						Database.executeUpdate(delMetAt);
 						Database.executeUpdate(delMeeting);
 						Database.executeUpdate(delUser);
 
@@ -198,13 +203,12 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 						this.dispose();
 
 					} else if (dialog2 == JOptionPane.NO_OPTION) {
-						Database.executeUpdate("DELETE FROM Post_Tag WHERE PostID in (SELECT PostID FROM Post WHERE UserID = " + tempId + ")");
-                                                Database.executeUpdate(delCom);
-                                                Database.executeUpdate(delPost);
-                                                Database.executeUpdate(delMetAt);
+						Database.executeUpdate("DELETE FROM Post_Tag WHERE PostID in (SELECT PostID FROM Post WHERE UserID = " + userID + ")");
+						Database.executeUpdate(delCom);
+						Database.executeUpdate(delPost);
+						Database.executeUpdate(delMetAt);
 						Database.executeUpdate(delMeeting);
 						Database.executeUpdate(delUser);
-						
 
 						JOptionPane.showMessageDialog(null, "Användaren och dess inlägg är nu borttagna");
 						new UpdateDeleteUser().setVisible(true);
@@ -218,28 +222,39 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 
 			}
 		}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			JOptionPane.showMessageDialog(null, "Var vänlig markera en användare");
+		}
     }//GEN-LAST:event_deleteButtonActionPerformed
 
-    private void jtfIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtfIDActionPerformed
-		// TODO add your handling code here:
-    }//GEN-LAST:event_jtfIDActionPerformed
-
     private void changeInformationButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changeInformationButtonActionPerformed
-
-		if (Validation.isInt(jtfID) && Validation.isStringNull(jtfID.getText())) {
-			int tempId = Integer.parseInt(jtfID.getText());
-			try {
-				String validId = Database.fetchSingle("SELECT UserID FROM User WHERE UserID =" + tempId);
-				if (validId != null) {
-					new EditUsers(tempId).setVisible(true);
-				} else {
-					JOptionPane.showMessageDialog(null, "Var god ange ett giltigt UserID!");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		int userID = -1;
+		try {
+			userID = Refactor.getTableValueFirstColumn(table2);
+			new EditUsers(userID).setVisible(true);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			JOptionPane.showMessageDialog(null, "Var vänlig markera en användare");
 		}
 
+		
+		//if (Validation.isInt(jtfID) && Validation.isStringNull(jtfID.getText())) {
+
+//			if (jtfID.getText().isBlank()) {
+//				userID = Refactor.getTableValueFirstColumn(table2);
+//			} else {
+//				userID = Integer.parseInt(jtfID.getText());
+//			}
+//			try {
+//				String validId = Database.fetchSingle("SELECT UserID FROM User WHERE UserID =" + userID);
+//				if (validId != null) {
+//					new EditUsers(userID).setVisible(true);
+//				} else {
+//					JOptionPane.showMessageDialog(null, "Var god ange ett giltigt UserID!");
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+		//}
 
     }//GEN-LAST:event_changeInformationButtonActionPerformed
 
@@ -249,15 +264,19 @@ public class UpdateDeleteUser extends javax.swing.JFrame
 
 	private boolean idKoll()
 	{
-		int tempId = Integer.parseInt(jtfID.getText());
+
 		try {
-			String validId = Database.fetchSingle("SELECT UserID FROM User WHERE UserID =" + tempId);
+			int userID = Refactor.getTableValueFirstColumn(table2);
+
+			String validId = Database.fetchSingle("SELECT UserID FROM User WHERE UserID =" + userID);
 			if (validId == null) {
 				JOptionPane.showMessageDialog(null, "Var god ange ett giltigt UserID!");
 				new UpdateDeleteUser().setVisible(true);
 				this.dispose();
 				return false;
 			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			JOptionPane.showMessageDialog(null, "Var vänlig markera en användare");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -272,6 +291,5 @@ public class UpdateDeleteUser extends javax.swing.JFrame
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jtfID;
     // End of variables declaration//GEN-END:variables
 }
