a77c17d3223a9fd3f5f8ca279d9cf485d98e18f7
kalasjonte <81684659+kalasjonte@users.noreply.github.com>
2021-04-21 15:05:24 +0200

    hejsan funkar nu?

--- src/projekt/frames/PersonalMeetings.java
+++ src/projekt/frames/PersonalMeetings.java
@@ -176,7 +176,7 @@ public class PersonalMeetings extends javax.swing.JFrame {
     }//GEN-LAST:event_jBBackActionPerformed
 
     private void jbNewMeetingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbNewMeetingActionPerformed
-
+        new NewMeeting(user).setVisible(true);
     }//GEN-LAST:event_jbNewMeetingActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
