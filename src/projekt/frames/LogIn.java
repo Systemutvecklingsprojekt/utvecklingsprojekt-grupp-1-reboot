8e45741403b947878ea6b3e5b81374b5b6bd1955
Josef Jamai-Haitot <jamaihaitot.j@gmail.com>
2021-04-23 11:33:55 +0200

    Update LogIn.java

--- src/projekt/frames/LogIn.java
+++ src/projekt/frames/LogIn.java
@@ -25,6 +25,8 @@ public class LogIn extends javax.swing.JFrame {
    
     public LogIn() {
         initComponents();
+        this.getRootPane().setDefaultButton(jBLogInButton);
+
     }
 
     /**
@@ -36,12 +38,16 @@ public class LogIn extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
         jBLogInButton = new javax.swing.JButton();
         jTFEmailField = new javax.swing.JTextField();
         jLEmailLabel = new javax.swing.JLabel();
         jLPasswordLabel = new javax.swing.JLabel();
         jPFPasswordField = new javax.swing.JPasswordField();
         jLWelcomeLabel = new javax.swing.JLabel();
+        jBackButton = new javax.swing.JButton();
+
+        jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -60,6 +66,14 @@ public class LogIn extends javax.swing.JFrame {
         jLWelcomeLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLWelcomeLabel.setText("Vänligen ange E-postadress och lösenord");
 
+        jBackButton.setBackground(new java.awt.Color(204, 204, 255));
+        jBackButton.setText("Tillbaka");
+        jBackButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBackButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -68,15 +82,19 @@ public class LogIn extends javax.swing.JFrame {
                 .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLWelcomeLabel)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLEmailLabel)
-                            .addComponent(jLPasswordLabel))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTFEmailField)
-                            .addComponent(jPFPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jBLogInButton, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jBackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jBLogInButton, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLEmailLabel)
+                                .addComponent(jLPasswordLabel))
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jTFEmailField)
+                                .addComponent(jPFPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(81, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -93,12 +111,19 @@ public class LogIn extends javax.swing.JFrame {
                     .addComponent(jPFPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLPasswordLabel))
                 .addGap(30, 30, 30)
-                .addComponent(jBLogInButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBLogInButton)
+                    .addComponent(jBackButton))
                 .addContainerGap(95, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jBackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBackButtonActionPerformed
+            new NotLoggedInHomeScreen().setVisible(true);
+            this.dispose();
+    }//GEN-LAST:event_jBackButtonActionPerformed
 
     private void jBLogInButtonActionPerformed(java.awt.event.ActionEvent evt) {                                              
         // TODO add your handling code here:
@@ -135,6 +160,8 @@ public class LogIn extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBLogInButton;
+    private javax.swing.JButton jBackButton;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLEmailLabel;
     private javax.swing.JLabel jLPasswordLabel;
     private javax.swing.JLabel jLWelcomeLabel;
