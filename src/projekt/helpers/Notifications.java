042dbc9ee5651f3d04225d0bb3840f53b4a8a48b
Noel Lindgren <apexlindgren@hotmail.com>
2021-04-26 14:36:12 +0200

    Notification utan kommentarer

--- src/projekt/helpers/Notifications.java
+++ src/projekt/helpers/Notifications.java
@@ -1,47 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projekt.helpers;
 
 import java.awt.*;
 import java.awt.TrayIcon.MessageType;
 
-/**
- *
- * @author albin
- */
-public class Notifications
-{
+public class Notifications {
 
-	public static void main(String[] args) throws AWTException
-	{
-		if (SystemTray.isSupported()) {
-			Notifications td = new Notifications();
-			td.displayTray();
-		} else {
-			System.err.println("System tray not supported!");
-		}
-	}
+    public static void main(String[] args) throws AWTException {
+        if (SystemTray.isSupported()) {
+            Notifications td = new Notifications();
+            td.displayTray();
+        } else {
+            System.err.println("System tray not supported!");
+        }
+    }
 
-	public void displayTray() throws AWTException
-	{
-		//Obtain only one instance of the SystemTray object
-		SystemTray tray = SystemTray.getSystemTray();
+    public void displayTray() throws AWTException {
+        //Obtain only one instance of the SystemTray object
+        SystemTray tray = SystemTray.getSystemTray();
 
-		//If the icon is a file
-		Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
-		//Alternative (if the icon is on the classpath):
-		//Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
+        //If the icon is a file
+        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
+        //Alternative (if the icon is on the classpath):
+        //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
 
-		TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-		//Let the system resize the image if needed
-		trayIcon.setImageAutoSize(true);
-		//Set tooltip text for the tray icon
-		trayIcon.setToolTip("System tray icon demo");
-		tray.add(trayIcon);
+        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
+        //Let the system resize the image if needed
+        trayIcon.setImageAutoSize(true);
+        //Set tooltip text for the tray icon
+        trayIcon.setToolTip("System tray icon demo");
+        tray.add(trayIcon);
 
-		trayIcon.displayMessage("Nytt möte!", "Du har möten som väntar på att bli accepterade.", MessageType.INFO);
-	}
+        trayIcon.displayMessage("Nytt möte!", "Du har möten som väntar på att bli accepterade.", MessageType.INFO);
+    }
 }
